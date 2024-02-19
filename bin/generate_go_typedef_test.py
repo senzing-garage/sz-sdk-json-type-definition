7681bfda9803db0dd8ee37ae6002a290ce26bbbd
docktermj <michael@dockter.com>
2024-02-19 18:20:22 -0500

    #15 Fix Go testing

--- bin/generate_go_typedef_test.py
+++ bin/generate_go_typedef_test.py
@@ -4,10 +4,11 @@
 Used to generate go/typedef_test.go
 """
 
-import logging
 import json
+import logging
 import os
 import re
+
 from test_cases import TEST_CASES
 
 IS_DEBUG = False
@@ -20,10 +21,11 @@ OUTPUT_FILE = "./go/typedef/typedef_test.go"
 # --- Helpers
 # -----------------------------------------------------------------------------
 
+
 def canonical_json(json_string):
     """Create compact JSON.  No spaces."""
     json_object = json.loads(json_string)
-    result = json.dumps(json_object, sort_keys=True, separators=(',', ':'))
+    result = json.dumps(json_object, sort_keys=True, separators=(",", ":"))
     return result
 
 
@@ -33,7 +35,7 @@ def canonical_json(json_string):
 
 # Set up logging.
 
-logging.basicConfig(format='%(asctime)s %(message)s', level=logging.INFO)
+logging.basicConfig(format="%(asctime)s %(message)s", level=logging.INFO)
 
 logging.info("{0}".format("-" * 80))
 logging.info("--- {0} - Begin".format(os.path.basename(__file__)))
@@ -84,10 +86,20 @@ with open(OUTPUT_FILE, "w", encoding="utf-8") as file:
     file.write(OUTPUT_HEADER)
     for senzing_api_class, method_test_cases in TEST_CASES.items():
         for test_case_name, test_case_json in method_test_cases.items():
-            better_test_case_name = re.sub('[^0-9a-zA-Z]+', '', test_case_name).capitalize()
+            better_test_case_name = re.sub(
+                "[^0-9a-zA-Z]+", "", test_case_name
+            ).capitalize()
             canonical_test_case_json = canonical_json(test_case_json)
-            file.write("func Test{0}{1}(test *testing.T) {{".format(senzing_api_class, better_test_case_name))
-            file.write(TEST_FUNCTION_TEMPLATE.format(json=canonical_test_case_json, struct=senzing_api_class, parens="{}"))
+            file.write(
+                "func Test{0}{1}(test *testing.T) {{".format(
+                    senzing_api_class, better_test_case_name
+                )
+            )
+            file.write(
+                TEST_FUNCTION_TEMPLATE.format(
+                    json=canonical_test_case_json, struct=senzing_api_class, parens="{}"
+                )
+            )
             file.write("}\n\n")
     file.write(OUTPUT_FOOTER)
 
