8cf41e2270b103a7b916008484fb5e8b0973aad2
docktermj <michael@dockter.com>
2024-09-04 09:01:32 -0400

    #21 Savepoint

---
+++ makefiles/linux.mk
@@ -0,0 +1,72 @@
+# Makefile extensions for linux.
+
+# -----------------------------------------------------------------------------
+# Variables
+# -----------------------------------------------------------------------------
+
+PATH := $(MAKEFILE_DIRECTORY)/bin:/$(HOME)/go/bin:$(PATH)
+
+# -----------------------------------------------------------------------------
+# OS specific targets
+# -----------------------------------------------------------------------------
+
+.PHONY: build-osarch-specific
+build-osarch-specific: linux/amd64
+
+
+.PHONY: clean-osarch-specific
+clean-osarch-specific:
+	@rm -f  $(GOPATH)/bin/$(PROGRAM_NAME) || true
+	@rm -f  $(MAKEFILE_DIRECTORY)/.coverage || true
+	@rm -f  $(MAKEFILE_DIRECTORY)/coverage.html || true
+	@rm -f  $(MAKEFILE_DIRECTORY)/coverage.out || true
+	@rm -f  $(MAKEFILE_DIRECTORY)/cover.out || true
+	@rm -fr $(TARGET_DIRECTORY) || true
+	@pkill godoc || true
+
+
+.PHONY: coverage-osarch-specific
+coverage-osarch-specific:
+	@go test -v -coverprofile=coverage.out -p 1 ./...
+	@go tool cover -html="coverage.out" -o coverage.html
+	@xdg-open $(MAKEFILE_DIRECTORY)/coverage.html
+
+
+.PHONY: dependencies-for-development-osarch-specific
+dependencies-for-development-osarch-specific: 
+	@curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(shell go env GOPATH)/bin v1.58.1
+
+
+.PHONY: documentation-osarch-specific
+documentation-osarch-specific:
+	@pkill godoc || true
+	@godoc &
+	@xdg-open http://localhost:6060
+
+
+.PHONY: hello-world-osarch-specific
+hello-world-osarch-specific:
+	$(info Hello World, from linux.)
+
+
+.PHONY: run-osarch-specific
+run-osarch-specific:
+	@go run main.go
+
+
+.PHONY: setup-osarch-specific
+setup-osarch-specific:
+	$(info No setup required.)
+
+
+.PHONY: test-osarch-specific
+test-osarch-specific:
+	@go test -json -v -p 1 ./... 2>&1 | tee /tmp/gotest.log | gotestfmt
+
+# -----------------------------------------------------------------------------
+# Makefile targets supported only by this platform.
+# -----------------------------------------------------------------------------
+
+.PHONY: only-linux
+only-linux:
+	$(info Only linux has this Makefile target.)
