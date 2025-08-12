9256e849574183af830119cbf412b7064f9e4264
Michael Dockter <michael@dockter.com>
2025-08-12 18:37:00 -0400

    #21 Savepoint

--- main.java
+++ main.java
@@ -1,22 +1,30 @@
 // # For more information, visit https://jsontypedef.com/docs/java-codegen/
 
-import com.senzing.schema.ConfigListDataSourcesResponse;
+import com.senzing.schema.SzConfigGetDataSourceRegistryResponse;
 import java.util.Map;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 class Main {
     public static void main(String[] args) {
+
+        // --------------------------------------------------------------------
+        // Demonstrate reconstructed JSON.
+        // --------------------------------------------------------------------
+
+        try {
         ObjectMapper objectMapper = new ObjectMapper();
         String jsonString = "{\"DATA_SOURCES\":[{\"DSRC_ID\":1,\"DSRC_CODE\":\"TEST\"},{\"DSRC_ID\":2,\"DSRC_CODE\":\"SEARCH\"}]}";
-        ConfigListDataSourcesResponse jsonStruct = objectMapper.readValue(jsonString,
-                ConfigListDataSourcesResponse.class);
+        SzConfigGetDataSourceRegistryResponse jsonStruct = objectMapper.readValue(jsonString,
+                SzConfigGetDataSourceRegistryResponse.class);
 
         // TODO: Loop through jsonStruct to print "ID: {0} Code: {1}"
 
-        jsonStruct.ConfigListDataSourcesResponse
 
         String reconstructedString = objectMapper.writeValueAsString(jsonString);
         System.out.printf("     Original JSON: %s%n", jsonString);
         System.out.printf("Reconstructed JSON: %s%n", reconstructedString);
+        } catch (Exception e) {
+
+        }
     }
 }
