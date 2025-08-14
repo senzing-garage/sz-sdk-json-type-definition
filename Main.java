7495cafe199f383083547fe0a2c0b5e5fe24b7a8
Michael Dockter <michael@dockter.com>
2025-08-14 10:37:49 -0400

    #21 Working java JSON reconstruction

--- Main.java
+++ Main.java
@@ -1,7 +1,11 @@
 // # For more information, visit https://jsontypedef.com/docs/java-codegen/
 
 import com.senzing.schema.SzConfigGetDataSourceRegistryResponse;
+import com.senzing.schema.GetDataSourceRegistry;
+import com.senzing.schema.DataSource;
+
 import java.util.Map;
+import java.util.List;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 class Main {
@@ -11,21 +15,34 @@ class Main {
         // Demonstrate reconstructed JSON.
         // --------------------------------------------------------------------
 
+        System.out.printf("\n--- Demonstrate reconstructed JSON --------------------------------------------\n");
+
         try {
-        ObjectMapper objectMapper = new ObjectMapper();
         String jsonString = "{\"DATA_SOURCES\":[{\"DSRC_ID\":1,\"DSRC_CODE\":\"TEST\"},{\"DSRC_ID\":2,\"DSRC_CODE\":\"SEARCH\"}]}";
+
+        // Unmarshall JSON string into a object.
+
+        ObjectMapper objectMapper = new ObjectMapper();
         SzConfigGetDataSourceRegistryResponse jsonStruct = objectMapper.readValue(jsonString,
                 SzConfigGetDataSourceRegistryResponse.class);
 
+        // Show individual (ID, Code) pairs.
 
-        bob = jsonStruct.DATA_SOURCES
-
-        // TODO: Loop through jsonStruct to print "ID: {0} Code: {1}"
+        GetDataSourceRegistry dataSourceRegistry = jsonStruct.getValue();
 
 
-        String reconstructedString = objectMapper.writeValueAsString(jsonString);
-        System.out.printf("     Original JSON: %s%n", jsonString);
-        System.out.printf("Reconstructed JSON: %s%n", reconstructedString);
+        for (DataSource dataSource : dataSourceRegistry.getDataSources()) {
+            System.out.printf("                ID: %s  Code: %s\n", dataSource.getDsrcId(), dataSource.getDsrcCode());
+        }
+
+        // Reconstruct JSON.
+
+        String reconstructedString = objectMapper.writeValueAsString(jsonStruct);
+
+        // Compare original and reconstructed JSON.
+
+        System.out.printf("     Original JSON: %s\n", jsonString);
+        System.out.printf("Reconstructed JSON: %s - notice JSON keys have been sorted.\n", reconstructedString);
         } catch (Exception e) {
 
         }
