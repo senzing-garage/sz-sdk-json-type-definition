86b37f0866a897daeb46d10d3311aa3d32de1a07
Michael Dockter <michael@dockter.com>
2025-08-14 18:59:31 -0400

    #21 Savepoint

--- csharp/Program.cs
+++ csharp/Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+using Newtonsoft.Json;
+
+var obj = new
+{
+    EmpID = "Larry",
+    Name = "Larry The Man",
+    Shift = new[] {
+        new { StartTime = "1 AM" },
+        new { StartTime = "2 PM" }
+    }
+};
+
+string json = JsonConvert.SerializeObject(obj);
+
 Console.WriteLine("Hello, World!");
