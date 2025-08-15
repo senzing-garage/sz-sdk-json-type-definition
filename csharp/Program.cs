a9cfdda9082bcdd57720ce955d242e884d538c48
Michael Dockter <michael@dockter.com>
2025-08-15 14:09:57 -0400

    #21 Fully working c-sharp

--- csharp/Program.cs
+++ csharp/Program.cs
@@ -13,14 +13,13 @@ namespace Bob
     {
         static void Main(string[] args)
         {
-
             Program myProgram = new Program();
 
             // ----------------------------------------------------------------
             // Demonstrate creating input parameter and parsing output result.
             // ----------------------------------------------------------------
 
-            Console.WriteLine("--- Demonstrate creating input parameter and parsing output result ------------\n\n");
+            Console.WriteLine("--- Demonstrate creating input parameter and parsing output result ------------\n");
 
             // Example of creating a JSON input parameter.
             // The advantage is that this is checked at compilation time.
@@ -53,27 +52,57 @@ namespace Bob
 
             string recordKeysJson = JsonSerializer.Serialize(recordKeyStruct);
 
-
-            SzEngineGetVirtualEntityByRecordIdRecordKeys? xyz = JsonSerializer.Deserialize<SzEngineGetVirtualEntityByRecordIdRecordKeys>(recordKeysJson);
-
-            Console.WriteLine(">>>>> {0:G}", xyz);
-
             // Simulate calling Senzing SDK.
 
             string response = myProgram.mockSzEngineGetVirtualEntityByRecordID(recordKeysJson, 1);
 
-            // Parse response.  FIXME;
+            // Parse response.
 
             SzEngineGetVirtualEntityByRecordIdResponse? virtualEntity = JsonSerializer.Deserialize<SzEngineGetVirtualEntityByRecordIdResponse>(response);
-
             if (virtualEntity != null)
             {
+                Console.WriteLine("RESOLVED_ENTITY.FEATURES['ADDRESS'][0].FEAT_DESC: {0:G}\n",
+                    virtualEntity.Value.ResolvedEntity.Features["ADDRESS"][0].FeatDesc);
 
-                // string x = virtualEntity.Value.ResolvedEntity.Features["ADDRESS"][0].FEAT_DESC;
-                string x = virtualEntity.Value.ResolvedEntity.Features["ADDRESS"][0].FeatDesc;
+                // Looping through list.
 
-                Console.WriteLine("RESOLVED_ENTITY.FEATURES['ADDRESS'][0].FEAT_DESC: {0:G}\n", x);
+                IList<FeatureForAttribute> addresses = virtualEntity.Value.ResolvedEntity.Features["ADDRESS"];
+                foreach (FeatureForAttribute address in addresses)
+                {
+                    Console.WriteLine("   ADDRESS FEAT_DESC: {0:G}", address.FeatDesc);
+                }
             }
+
+            // ----------------------------------------------------------------
+            // Demonstrate reconstructed JSON.
+            // ----------------------------------------------------------------
+
+            Console.WriteLine("\n--- Demonstrate reconstructed JSON --------------------------------------------\n");
+            string jsonString = "{\"DATA_SOURCES\":[{\"DSRC_ID\":1,\"DSRC_CODE\":\"TEST\"},{\"DSRC_ID\":2,\"DSRC_CODE\":\"SEARCH\"}]}";
+
+            // Unmarshall JSON string into a structure.
+
+            SzConfigGetDataSourceRegistryResponse? dataSourceRegistry = JsonSerializer.Deserialize<SzConfigGetDataSourceRegistryResponse>(jsonString);
+
+            // Show individual (ID, Code) pairs.
+
+            if (dataSourceRegistry != null)
+            {
+                foreach (DataSource datasource in dataSourceRegistry.Value.DataSources)
+                {
+                    Console.WriteLine("                ID: {0:G}  Code: {1:G}", datasource.DsrcId, datasource.DsrcCode);
+                }
+            }
+
+            // Reconstruct JSON.
+
+            string reconstructedString = JsonSerializer.Serialize(dataSourceRegistry);
+
+            // Compare original and reconstructed JSON.
+
+            Console.WriteLine("     Original JSON: {0:G}", jsonString);
+            Console.WriteLine("Reconstructed JSON: {0:G} - notice JSON keys have been sorted.\n", reconstructedString);
+
         }
 
         public string mockSzEngineGetVirtualEntityByRecordID(string recordKeys, int flags)
