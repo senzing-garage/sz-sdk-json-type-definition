cb2ed53a1e0981816b6aeca70a8e5a3db73f52f8
docktermj <michael@dockter.com>
2025-09-05 16:30:03 -0400

    #21 Savepoint

---
+++ bin/generate_json_responses.py
@@ -0,0 +1,189 @@
+#! /usr/bin/env python3
+
+# pylint: disable=duplicate-code
+
+import json
+
+"""
+For more information, visit https://jsontypedef.com/docs/python-codegen/
+"""
+
+GLOBAL_OUTPUT_DIRECTORY = "./docs/json-responses"
+GLOBAL_JSON_KEYS = [
+    "SzConfigExportResponse",
+    "SzConfigGetDataSourceRegistryResponse",
+    "SzConfigManagerGetConfigRegistryResponse",
+    "SzConfigRegisterDataSourceResponse",
+    "SzConfigUnregisterDataSourceResponse",
+    "SzDiagnosticCheckRepositoryPerformanceResponse",
+    "SzDiagnosticGetFeatureResponse",
+    "SzDiagnosticGetRepositoryInfoResponse",
+    "SzEngineAddRecordResponse",
+    "SzEngineDeleteRecordResponse",
+    "SzEngineExportCsvEntityReportCsvColumnList",
+    "SzEngineFetchNextResponse",
+    "SzEngineFindInterestingEntitiesByEntityIdResponse",
+    "SzEngineFindInterestingEntitiesByRecordIdResponse",
+    "SzEngineFindNetworkByEntityIdEntityIds",
+    "SzEngineFindNetworkByEntityIdResponse",
+    "SzEngineFindNetworkByRecordIdRecordKeys",
+    "SzEngineFindNetworkByRecordIdResponse",
+    "SzEngineFindPathByEntityIdAvoidEntityIds",
+    "SzEngineFindPathByEntityIdRequiredDataSources",
+    "SzEngineFindPathByEntityIdResponse",
+    "SzEngineFindPathByRecordIdAvoidRecordKeys",
+    "SzEngineFindPathByRecordIdRequiredDataSources",
+    "SzEngineFindPathByRecordIdResponse",
+    "SzEngineGetEntityByEntityIdResponse",
+    "SzEngineGetEntityByRecordIdResponse",
+    "SzEngineGetRecordPreviewResponse",
+    "SzEngineGetRecordResponse",
+    "SzEngineGetRedoRecordResponse",
+    "SzEngineGetStatsResponse",
+    "SzEngineGetVirtualEntityByRecordIdRecordKeys",
+    "SzEngineGetVirtualEntityByRecordIdResponse",
+    "SzEngineHowEntityByEntityIdResponse",
+    "SzEngineProcessRedoRecordResponse",
+    "SzEngineReevaluateEntityResponse",
+    "SzEngineReevaluateRecordResponse",
+    "SzEngineSearchByAttributesAttributes",
+    "SzEngineSearchByAttributesResponse",
+    "SzEngineSearchByAttributesSearchProfile",
+    "SzEngineStreamExportJsonEntityReportResponse",
+    "SzEngineWhyEntitiesResponse",
+    "SzEngineWhyRecordInEntityResponse",
+    "SzEngineWhyRecordsResponse",
+    "SzEngineWhySearchAttributes",
+    "SzEngineWhySearchResponse",
+    "SzEngineWhySearchSearchProfile",
+    "SzProductGetLicenseResponse",
+    "SzProductGetVersionResponse",
+]
+
+DEFINITIONS = {}
+
+
+def handle_elements(json_value):
+    elements = json_value.get("elements", {})
+    result = recurse(elements)
+    return [result]
+
+
+def handle_metadata(json_value):
+    metadata = json_value.get("metadata")
+    python_type = metadata.get("pythonType")
+    if python_type:
+        return handle_python_type(python_type)
+    return None
+
+
+def handle_properties(json_value):
+    result = {}
+    properties = json_value.get("properties", {})
+    for key, value in properties.items():
+        result[key] = recurse(value)
+    return result
+
+
+def handle_python_type(python_type):
+    result = {}
+
+    match python_type:
+        case "Dict[str, FeatureScoresForAttribute]":
+            return {
+                "<user_defined_json_key>": recurse(
+                    DEFINITIONS.get("FeatureScoresForAttribute")
+                )
+            }
+        case "Dict[str, List[FeatureForAttribute]]":
+            return {
+                "<user_defined_json_key>": [
+                    recurse(DEFINITIONS.get("FeatureForAttribute"))
+                ]
+            }
+        case "Dict[str, List[FeatureForAttributeWithAttributes]]":
+            return {
+                "<user_defined_json_key>": [
+                    recurse(DEFINITIONS.get("FeatureForAttributeWithAttributes"))
+                ]
+            }
+        case "Dict[str, List[FeatureForGetEntity]]":
+            return {
+                "<user_defined_json_key>": [
+                    recurse(DEFINITIONS.get("FeatureForGetEntity"))
+                ]
+            }
+        case "Dict[str, List[MatchInfoForAttribute]]":
+            return {
+                "<user_defined_json_key>": [
+                    recurse(DEFINITIONS.get("MatchInfoForAttribute"))
+                ]
+            }
+        case "Dict[str, object]":
+            return {
+                "<user_record_json_key_1>": "user_record_json_value_1",
+                "<user_record_json_key_2>": "user_record_json_value_2",
+            }
+        case _:
+            print(f"Error: Bad 'pythonType:' {python_type}")
+            raise NotImplementedError
+    return result
+
+
+def handle_ref(json_value):
+    return recurse(DEFINITIONS.get(json_value.get("ref")))
+
+
+def handle_type(json_value):
+    return json_value.get("type")
+
+
+def recurse(json_value):
+    if "metadata" in json_value:
+        result = handle_metadata(json_value)
+        if result:
+            return result
+
+    if "type" in json_value:
+        return handle_type(json_value)
+
+    if "ref" in json_value:
+        return handle_ref(json_value)
+
+    if "properties" in json_value:
+        return handle_properties(json_value)
+
+    if "elements" in json_value:
+        return handle_elements(json_value)
+
+    return {}
+
+
+# -----------------------------------------------------------------------------
+# --- Main
+# -----------------------------------------------------------------------------
+
+# Read JSON from file.
+
+INPUT_FILENAME = "./senzingapi-RFC8927.json"
+with open(INPUT_FILENAME, "r", encoding="utf-8") as input_file:
+    DATA = json.load(input_file)
+
+DEFINITIONS = DATA.get("definitions", {})
+
+# Recurse through dictionary.
+
+for requested_json_key in GLOBAL_JSON_KEYS:
+    json_value = DEFINITIONS.get(requested_json_key)
+
+    # Short-circuit when JSON key not found.
+
+    if json_value is None:
+        print(f"Could not find JSON key: {requested_json_key}")
+        continue
+
+    result = recurse(json_value)
+
+    output_file = f"{GLOBAL_OUTPUT_DIRECTORY}/{requested_json_key}.json"
+    with open(output_file, "w", encoding="utf-8") as json_file:
+        json.dump(result, json_file, indent=4)
---
+++ bin/test_using_senzing.py
@@ -0,0 +1,286 @@
+#! /usr/bin/env python3
+
+# pylint: disable=duplicate-code
+
+"""
+For more information, visit https://jsontypedef.com/docs/python-codegen/
+"""
+
+import json
+import os
+import pathlib
+import subprocess
+
+from python.typedef import (
+    SzEngineAddRecordResponse,
+    SzEngineDeleteRecordResponse,
+    SzEngineFindNetworkByEntityIDResponse,
+    SzEngineFindNetworkByRecordIDResponse,
+    SzEngineFindPathByEntityIDResponse,
+    SzEngineFindPathByRecordIDResponse,
+    SzEngineGetEntityByEntityIDResponse,
+    SzEngineGetEntityByRecordIDResponse,
+    SzEngineGetRecordResponse,
+    SzEngineGetVirtualEntityByRecordIDResponse,
+)
+
+# -----------------------------------------------------------------------------
+# Utility functions
+# -----------------------------------------------------------------------------
+
+
+def path_to_testdata(filename: str) -> str:
+    current_path = pathlib.Path(__file__).parent.resolve()
+    result = os.path.abspath("{0}/testdata/{1}".format(current_path, filename))
+    return result
+
+
+# -----------------------------------------------------------------------------
+# Tests
+# -----------------------------------------------------------------------------
+
+
+def test_szengine_add_record_with_info_005():
+    with open(
+        path_to_testdata("SzEngineAddRecordResponse-test-005.json"),
+        encoding="utf-8",
+    ) as input_file:
+        response = SzEngineAddRecordResponse.from_json_data(json.load(input_file))
+    assert response.data_source == "TEST"
+    assert response.record_id == "WITH_INFO_1"
+    assert response.affected_entities[0].entity_id == 7
+
+
+def test_szengine_delete_record_with_info_002():
+    with open(
+        path_to_testdata("SzEngineDeleteRecordResponse-test-002.json"),
+        encoding="utf-8",
+    ) as input_file:
+        response = SzEngineDeleteRecordResponse.from_json_data(json.load(input_file))
+    assert response.data_source == "TEST"
+    assert response.record_id == "DELETE_TEST"
+    assert response.affected_entities[0].entity_id == 100002
+
+
+def test_szengine_find_network_by_entity_id_001():
+    with open(
+        path_to_testdata("SzEngineFindNetworkByEntityIdResponse-test-001.json"),
+        encoding="utf-8",
+    ) as input_file:
+        response = SzEngineFindNetworkByEntityIDResponse.from_json_data(
+            json.load(input_file)
+        )
+    assert response.entity_paths == []
+    assert response.entities[0].resolved_entity.entity_id == 1
+    assert response.entities[0].resolved_entity.entity_name == "Robert Smith"
+    assert (
+        response.entities[0].resolved_entity.record_summary[0].data_source
+        == "CUSTOMERS"
+    )
+
+
+def test_szengine_find_network_by_record_id_001():
+    with open(
+        path_to_testdata("SzEngineFindNetworkByRecordIdResponse-test-001.json"),
+        encoding="utf-8",
+    ) as input_file:
+        response = SzEngineFindNetworkByRecordIDResponse.from_json_data(
+            json.load(input_file)
+        )
+    assert response.entity_paths == []
+    assert response.entities[0].resolved_entity.entity_id == 1
+    assert response.entities[0].resolved_entity.entity_name == "Robert Smith"
+    assert (
+        response.entities[0].resolved_entity.record_summary[0].data_source
+        == "CUSTOMERS"
+    )
+
+
+def test_szengine_find_path_by_entity_id_001():
+    with open(
+        path_to_testdata("SzEngineFindPathByEntityIdResponse-test-001.json"),
+        encoding="utf-8",
+    ) as input_file:
+        response = SzEngineFindPathByEntityIDResponse.from_json_data(
+            json.load(input_file)
+        )
+    assert response.entity_paths[0].start_entity_id == 1
+    assert response.entities[0].related_entities == []
+    assert response.entities[0].resolved_entity.entity_id == 1
+    assert response.entities[0].resolved_entity.entity_name == "Robert Smith"
+    assert response.entities[0].resolved_entity.record_summary[0].record_count == 3
+
+
+def test_szengine_find_path_by_record_id_001():
+    with open(
+        path_to_testdata("SzEngineFindPathByRecordIdResponse-test-001.json"),
+        encoding="utf-8",
+    ) as input_file:
+        response = SzEngineFindPathByRecordIDResponse.from_json_data(
+            json.load(input_file)
+        )
+    assert response.entity_paths[0].start_entity_id == 1
+    assert response.entities[0].related_entities == []
+    assert response.entities[0].resolved_entity.entity_id == 1
+    assert response.entities[0].resolved_entity.entity_name == "Robert Smith"
+    assert response.entities[0].resolved_entity.record_summary[0].record_count == 3
+
+
+# def test_szengine_get_entity_by_entity_id_01():
+#     with open(
+#         path_to_testdata("SzEngineGetEntityByEntityIdResponse-test-001.json"),
+#         encoding="utf-8",
+#     ) as input_file:
+#         response = SzEngineGetEntityByEntityIDResponse.from_json_data(
+#             json.load(input_file)
+#         )
+#     assert response.related_entities == []
+#     assert response.resolved_entity.entity_id == 1
+#     assert response.resolved_entity.entity_name == "Robert Smith"
+#     feature_json = response.resolved_entity.features["ADDRESS"][0].feat_desc
+#     feature = FeatureForAttribute.from_json_data(feature_json)
+#     assert feature.feat_desc == "1515 Adela Lane Las Vegas NV 89111"
+#     assert feature.feat_desc_values[0].lib_feat_id == 20
+
+
+def test_szengine_get_entity_by_entity_id_001():
+    with open(
+        path_to_testdata("SzEngineGetEntityByEntityIdResponse-test-001.json"),
+        encoding="utf-8",
+    ) as input_file:
+        response = SzEngineGetEntityByEntityIDResponse.from_json_data(
+            json.load(input_file)
+        )
+    assert response.related_entities == []
+    assert response.resolved_entity.entity_id == 1
+    assert response.resolved_entity.entity_name == "Robert Smith"
+    assert (
+        response.resolved_entity.features["ADDRESS"][0].feat_desc
+        == "1515 Adela Lane Las Vegas NV 89111"
+    )
+    assert response.resolved_entity.features["ADDRESS"][0].lib_feat_id == 20
+
+
+def test_szengine_get_entity_by_entity_id_003():
+    with open(
+        path_to_testdata("SzEngineGetEntityByEntityIdResponse-test-003.json"),
+        encoding="utf-8",
+    ) as input_file:
+        response = SzEngineGetEntityByEntityIDResponse.from_json_data(
+            json.load(input_file)
+        )
+    assert response.related_entities == []
+    assert response.resolved_entity.entity_id == 1
+    assert response.resolved_entity.entity_name == "blank"
+    assert response.resolved_entity.features["ADDRESS"][0].feat_desc == "blank"
+    assert (
+        response.resolved_entity.features["ADDRESS"][0].feat_desc_values[0].lib_feat_id
+        == 1
+    )
+
+
+def test_szengine_get_entity_by_record_id_001():
+    with open(
+        path_to_testdata("SzEngineGetEntityByRecordIdResponse-test-001.json"),
+        encoding="utf-8",
+    ) as input_file:
+        response = SzEngineGetEntityByRecordIDResponse.from_json_data(
+            json.load(input_file)
+        )
+    assert response.related_entities == []
+    assert response.resolved_entity.entity_id == 1
+    assert response.resolved_entity.entity_name == "Robert Smith"
+    assert (
+        response.resolved_entity.features["ADDRESS"][0].feat_desc
+        == "1515 Adela Lane Las Vegas NV 89111"
+    )
+    assert (
+        response.resolved_entity.features["ADDRESS"][0].feat_desc_values[0].lib_feat_id
+        == 20
+    )
+
+
+def test_szengine_get_record_003():
+    with open(
+        path_to_testdata("SzEngineGetRecordResponse-test-003.json"), encoding="utf-8"
+    ) as input_file:
+        response = SzEngineGetRecordResponse.from_json_data(json.load(input_file))
+    assert response.data_source == "CUSTOMERS"
+    assert response.record_id == "1001"
+    assert isinstance(response.json_data, dict)
+    assert response.json_data["DATA_SOURCE"] == "CUSTOMERS"
+    assert response.json_data["RECORD_ID"] == "1001"
+
+
+# TODO: Fix this
+# def test_szengine_get_redo_record_014():
+#     with open(
+#         path_to_testdata("SzEngineGetRedoRecordResponse-test-001.json"),
+#         encoding="utf-8",
+#     ) as input_file:
+#         response = SzEngineGetRedoRecordResponse.from_json_data(json.load(input_file))
+#     assert response.value == {}
+
+
+def test_szengine_get_virtual_entity_by_record_id_001():
+    with open(
+        path_to_testdata("SzEngineGetVirtualEntityByRecordIdResponse-test-001.json"),
+        encoding="utf-8",
+    ) as input_file:
+        response = SzEngineGetVirtualEntityByRecordIDResponse.from_json_data(
+            json.load(input_file)
+        )
+    assert response.resolved_entity.entity_id == 1
+    assert response.resolved_entity.entity_name == "Robert Smith"
+    assert response.resolved_entity.features["NAME"][0].feat_desc == "Robert Smith"
+    assert response.resolved_entity.features["NAME"][0].lib_feat_id == 1
+    assert (
+        response.resolved_entity.features["NAME"][0].feat_desc_values[1].lib_feat_id
+        == 18
+    )
+
+
+# -----------------------------------------------------------------------------
+# Utility functions
+# -----------------------------------------------------------------------------
+
+
+def infer_json_type_definition(example_json: str) -> str:
+
+    try:
+        cmd_echo_result = subprocess.run(
+            ["echo", example_json],
+            capture_output=True,
+            text=True,
+            check=True,
+        )
+
+        cmd_jtd_infer_result = subprocess.run(
+            ["jtd-infer"],
+            input=cmd_echo_result.stdout,
+            capture_output=True,
+            text=True,
+            check=True,
+        )
+
+        return cmd_jtd_infer_result.stdout
+    except subprocess.CalledProcessError as e:
+        print(f"Error executing command: {e}")
+        print(f"Stderr: {e.stderr}")
+    except FileNotFoundError:
+        print("Error: The specified command was not found.")
+
+
+# -----------------------------------------------------------------------------
+# Main
+# -----------------------------------------------------------------------------
+
+if __name__ == "__main__":
+
+    try:
+        example_json = '{"DATA_SOURCES":[{"DSRC_CODE":"blank","DSRC_ID":1},{"DSRC_CODE":"blank","DSRC_ID":1},{"DSRC_CODE":"blank","DSRC_ID":1},{"DSRC_CODE":"blank","DSRC_ID":1},{"DSRC_CODE":"blank","DSRC_ID":1},{"DSRC_CODE":"blank","DSRC_ID":1},{"DSRC_CODE":"blank","DSRC_ID":1},{"DSRC_CODE":"blank","DSRC_ID":1}]}'
+        xschema = infer_json_type_definition(example_json)
+        print(xschema)
+
+
+    test_szengine_add_record_with_info_005()
